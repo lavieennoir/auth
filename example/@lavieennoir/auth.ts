89f6f9f53f4fd9fd6f888cc9af01801ebfec50f7
lavieennoir <dmitriykhiznyak@gmail.com>
2023-04-10 17:02:33 +0300

    feat: update example project exports

--- example/@lavieennoir/auth.ts
+++
@@ -1,3 +0,0 @@
-export { default } from '../../src';
-export * from '../../src';
-// This file is a mock of '@lavieennoir/auth' package
---
+++ example/@lavieennoir/auth/react.ts
@@ -0,0 +1,2 @@
+export * from '../../../src/react';
+// This file is a mock of '@lavieennoir/auth/react' package
